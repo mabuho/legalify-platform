72a1571e5f63430877d89ca43e0414b79c836524
Fernando Fernández <m.snowman@gmail.com>
2025-06-04 18:46:09 -0600

    Initial oAuth setup

---
+++ legalify-auth-service/src/main/java/com/legalify/auth/model/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.legalify.auth.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String email;
+    private String password;
+}
